af1787ad9f1678eabcfd3f9ac025073e02fd3c26
Alexander Makar <makaralex2013@gmail.com>
2021-02-14 18:24:56 +0300

    Creating the Ral Repository

---
+++ BethanysPieShop/Models/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public CategoryRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+        public IEnumerable<Category> AllCategories
+        {
+            get
+            {
+                return appDbContext.Categories;
+            }
+        }
+    }
+}
